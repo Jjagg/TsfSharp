c7bbded7d6c7b5ddab8d828bf77c7ebdbe9c7e14
Jesse Gielen <jessegielen@hotmail.com>
2020-07-31 11:43:45 +0200

    Generate bindings

---
+++ src/StgMedium.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using SharpGen.Runtime;
+
+namespace TsfSharp
+{
+    public struct StgMedium
+    {
+        public int tymed;
+        public IntPtr handle;
+        public IntPtr pUnkForRelease;
+    }
+}
